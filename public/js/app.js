c2ea7e17accf79259e0e590eb61ec311e3bbdef3
Shridhar <kalukheshridhar24@gmail.com>
2021-07-02 22:15:44 +0530

    second commit

--- public/js/app.js
+++ public/js/app.js
@@ -10,7 +10,8 @@ weatherForm.addEventListener('submit', (e)=>{
     messageOne.textContent = 'Loading...'
     messageTwo.textContent = ''
     const location = search.value;
-    fetch(`http://localhost:3000/weather?address=${location}`).then((res)=>{
+
+    fetch(`weather?address=${location}`).then((res)=>{
         res.json().then((data)=>{
             if (data.err){
                 messageOne.textContent = `${data.err}`
